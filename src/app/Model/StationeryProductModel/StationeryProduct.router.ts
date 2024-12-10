8200ed86b128bdaf31ce62f014f5dcf45a135081
Md-Abdur-Razzak <razzak445444@gmail.com>
2024-12-10 12:06:05 +0600

    j

--- src/app/Model/StationeryProductModel/StationeryProduct.router.ts
+++ src/app/Model/StationeryProductModel/StationeryProduct.router.ts
@@ -1,9 +1,25 @@
-import express from 'express';
+import express, { Request,Response ,NewableFunction} from 'express';
 import { stonaryController } from './StationeryProduct.controller';
+import StationeryProductSchemazod from './Stonary.zod.validation';
+import { AnyZodObject } from 'zod';
 
 const router = express.Router();
 
-router.post('/products', stonaryController.stonaryCreatController);
+const prodectMiddleware = (sheama:AnyZodObject)=>{
+ return async (req:Request,res:Response,next:NewableFunction)=>{
+  try {
+    await sheama.parseAsync({
+      body:req.body
+    })
+  } catch (error) {
+    next(error)
+  }
+
+  return next()
+}
+
+}
+router.post('/products',prodectMiddleware(StationeryProductSchemazod), stonaryController.stonaryCreatController);
 router.get('/products', stonaryController.getAllStanaryProduct);
 router.get('/products/:productId', stonaryController.singleProductController);
 router.put('/products/:productId', stonaryController.updateProductControllers);
